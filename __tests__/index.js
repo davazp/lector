8a62a2e02752b3624a9d7db10b20069ca7b3f2d5
David Vázquez Púa <davazp@gmail.com>
2017-11-29 23:26:13 +0100

    Use arrow functions for the tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,25 +1,25 @@
 const Reader = require("../");
 const { ask } = Reader;
 
-test("identity reader returns the context", async function() {
+test("identity reader returns the context", async () => {
   const result = await ask.run({ x: 10 });
   expect(result).toEqual({ x: 10 });
 });
 
-test("readers compose properly", async function() {
+test("readers compose properly", async () => {
   const x = ask.then(c => c.x);
   const y = x.then(x => x.y);
   const result = await y.run({ x: { y: 2 } });
   expect(result).toBe(2);
 });
 
-test("readers .then method can return other readers", async function() {
+test("readers .then method can return other readers", async () => {
   const r = ask.then(c => Reader.of(c * c));
   const result = await r.run(10);
   expect(result).toBe(100);
 });
 
-test("readers .prop method can access a property from the previous result", async function() {
+test("readers .prop method can access a property from the previous result", async () => {
   const r = ask.prop("x").prop("y");
   const result = await r.run({ x: { y: 42 } });
   expect(result).toBe(42);
