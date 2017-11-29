dbdc2c3c457a430d72cb16e5004c31f6e0d877ec
David Vázquez Púa <davazp@gmail.com>
2017-11-29 22:45:52 +0100

    Format code with prettier

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 class Reader {
-
   // run :: Context -> Promise<result>
-  constructor (run) {
-    this.run = run
+  constructor(run) {
+    this.run = run;
   }
 
-  then (fn){
-    return new Reader(context=>{
-      return this.run(context).then(result=>{
-        const next = Reader.of( fn(result) )
-        return next.run(context)
-      })
-    })
+  then(fn) {
+    return new Reader(context => {
+      return this.run(context).then(result => {
+        const next = Reader.of(fn(result));
+        return next.run(context);
+      });
+    });
   }
 
-  static of (x) {
-    return x instanceof Reader? x: new Reader(_=> Promise.resolve(x) )
+  static of(x) {
+    return x instanceof Reader ? x : new Reader(_ => Promise.resolve(x));
   }
-
 }
 
-Reader.ask = new Reader(c => Promise.resolve(c) )
-module.exports = Reader
+Reader.ask = new Reader(c => Promise.resolve(c));
+module.exports = Reader;
