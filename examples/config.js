dbad4b4e6066bbf6343dc16b15f30f155ad62745
David Vázquez Púa <davazp@gmail.com>
2017-11-30 18:37:16 +0100

    Add an example about how this could be used

---
+++ examples/config.js
@@ -0,0 +1,46 @@
+const Promise = require("bluebird");
+const { ask, coroutine } = require("../");
+
+// Basic context
+const config = ask.prop("config");
+
+// Abstract selectors out of the general context
+const userPreferences = config.then(c => {
+  return Object.assign({ language: "en" }, c);
+});
+
+const userLanguage = userPreferences.prop("language");
+
+// You can also create functions with arguments
+const translate = key => {
+  const i18n = {
+    en: { hello: "hello", bye: "bye" },
+    es: { hello: "hola", bye: "adios" }
+  };
+  return userLanguage.then(lang => i18n[lang][key]);
+};
+
+const main = coroutine(function*() {
+  console.log(yield translate("hello"), "!");
+  yield Promise.delay(1000);
+  console.log(yield translate("bye"), "!");
+});
+
+// User your functions by providing the context once
+
+const _context1 = {
+  // No language set, should use the default
+};
+const _context2 = {
+  config: { language: "es" }
+};
+
+const test = () => {
+  return main()
+    .run(_context1)
+    .then(() => {
+      main().run(_context2);
+    });
+};
+
+test();
